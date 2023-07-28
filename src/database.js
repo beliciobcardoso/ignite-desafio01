ac3193b6676bd3cad1e9590a36c048538dc878ff
Belicio Batista Cardoso <belloinfo@gmail.com>
2023-07-28 15:47:26 -0300

    Create database.js

---
+++ src/database.js
@@ -0,0 +1,63 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL('../db.json', import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch((erro) => {
+        this.#pesist();
+        console.log(erro);
+      });
+  }
+
+  #pesist = async () => {
+    await fs.writeFile(databasePath, JSON.stringify(this.#database));
+  };
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#pesist();
+
+    return data;
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex === -1) {
+      return false;
+    }
+
+    this.#database[table].splice(rowIndex, 1);
+    this.#pesist();
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex === -1) {
+      return false;
+    }
+
+    this.#database[table][rowIndex] = { id, ...data };
+
+    this.#pesist();
+  }
+}
